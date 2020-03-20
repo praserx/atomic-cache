711f10f340cdf98afe9db8aa3ad29df1a54df2e7
PraserX <tpekar@volny.cz>
2020-03-20 19:02:14 +0100

    Added BTree index, multiple shards sections

--- cache_options.go
+++ cache_options.go
@@ -2,12 +2,20 @@ package atomiccache
 
 // Options are used for AtomicCache construct function.
 type Options struct {
-	// Size of byte array used for memory allocation.
-	RecordSize uint32
+	// Size of byte array used for memory allocation at small shard section.
+	RecordSizeSmall uint32
+	// Size of byte array used for memory allocation at medium shard section.
+	RecordSizeMedium uint32
+	// Size of byte array used for memory allocation at large shard section.
+	RecordSizeLarge uint32
 	// Maximum records per shard.
 	MaxRecords uint32
-	// Maximum shards which can be allocated in cache memory.
-	MaxShards uint32
+	// Maximum small shards which can be allocated in cache memory.
+	MaxShardsSmall uint32
+	// Maximum medium shards which can be allocated in cache memory.
+	MaxShardsMedium uint32
+	// Maximum large shards which can be allocated in cache memory.
+	MaxShardsLarge uint32
 	// Garbage collector starter (run garbage collection every X sets).
 	GcStarter uint32
 }
@@ -15,10 +23,24 @@ type Options struct {
 // Option specification for Printer package.
 type Option func(*Options)
 
-// OptionRecordSize option specification.
-func OptionRecordSize(option uint32) Option {
+// OptionRecordSizeSmall option specification.
+func OptionRecordSizeSmall(option uint32) Option {
 	return func(opts *Options) {
-		opts.RecordSize = option
+		opts.RecordSizeSmall = option
+	}
+}
+
+// OptionRecordSizeMedium option specification.
+func OptionRecordSizeMedium(option uint32) Option {
+	return func(opts *Options) {
+		opts.RecordSizeMedium = option
+	}
+}
+
+// OptionRecordSizeLarge option specification.
+func OptionRecordSizeLarge(option uint32) Option {
+	return func(opts *Options) {
+		opts.RecordSizeLarge = option
 	}
 }
 
@@ -29,10 +51,24 @@ func OptionMaxRecords(option uint32) Option {
 	}
 }
 
-// OptionMaxShards option specification.
-func OptionMaxShards(option uint32) Option {
+// OptionMaxShardsSmall option specification.
+func OptionMaxShardsSmall(option uint32) Option {
 	return func(opts *Options) {
-		opts.MaxShards = option
+		opts.MaxShardsSmall = option
+	}
+}
+
+// OptionMaxShardsMedium option specification.
+func OptionMaxShardsMedium(option uint32) Option {
+	return func(opts *Options) {
+		opts.MaxShardsMedium = option
+	}
+}
+
+// OptionMaxShardsLarge option specification.
+func OptionMaxShardsLarge(option uint32) Option {
+	return func(opts *Options) {
+		opts.MaxShardsLarge = option
 	}
 }
 
--- cache_test.go
+++ cache_test.go
@@ -1,24 +1,43 @@
 package atomiccache
 
 import (
-	"reflect"
-	"time"
 	"encoding/binary"
+	"math/rand"
+	"reflect"
+	"strconv"
 	"testing"
+	"time"
+
+	big "github.com/allegro/bigcache"
+	fre "github.com/coocood/freecache"
+	has "github.com/hashicorp/golang-lru"
 )
+
+func TestCacheFuncGetShardsSectionBySize(t *testing.T) {
+	for _, c := range []struct {
+		in   int
+		want int
+	}{
+		{256, 1}, {512, 1}, {2048, 2}, {8127, 3},
+	} {
+		cache := New()
+
+		_, shardSectionID := cache.getShardsSectionBySize(c.in)
+		if !reflect.DeepEqual(shardSectionID, uint8(c.want)) {
+			t.Errorf("%v != %v", c.in, c.want)
+		}
+	}
+}
 
 func TestCacheSimple(t *testing.T) {
 	for i, c := range []struct {
-		recordCount uint32
-		recordSize  uint32
-		shardCount  uint32
-		in          []byte
-		want        []byte
+		in   []byte
+		want []byte
 	}{
-		{4096, 4096, 128, []byte{0}, []byte{0}},
-		{4096, 4096, 128, []byte{0, 1, 2, 3, 4, 5}, []byte{0, 1, 2, 3, 4, 5}},
+		{[]byte{0}, []byte{0}},
+		{[]byte{0, 1, 2, 3, 4, 5}, []byte{0, 1, 2, 3, 4, 5}},
 	} {
-		cache := New(OptionMaxRecords(c.recordCount), OptionRecordSize(c.recordSize), OptionMaxShards(c.shardCount))
+		cache := New()
 		if err := cache.Set([]byte{byte(i)}, c.in, 0); err != nil {
 			t.Errorf("Set error: %s", err.Error())
 		}
@@ -34,27 +53,51 @@ func TestCacheSimple(t *testing.T) {
 	}
 }
 
+func TestCacheDiffSizeData(t *testing.T) {
+	for _, c := range []struct {
+		in int
+	}{
+		{256}, {512}, {513}, {2047}, {2048}, {2049}, {4096}, {8127}, {8128},
+	} {
+		var bigString string
+		for x := 0; x < c.in; x++ {
+			bigString += "x"
+		}
+
+		cache := New()
+		if err := cache.Set([]byte{byte(0)}, []byte(bigString), 0); err != nil {
+			t.Errorf("Set error: %s", err.Error())
+		}
+
+		value, err := cache.Get([]byte{byte(0)})
+		if err != nil {
+			t.Errorf("Get error: %s", err.Error())
+		}
+
+		if !reflect.DeepEqual(string(value), bigString) {
+			t.Errorf("%v != %v", string(value), bigString)
+		}
+	}
+}
+
 func TestCacheIntermediate(t *testing.T) {
 	for _, c := range []struct {
-		recordCount uint32
-		recordSize  uint32
-		shardCount  uint32
-		in          []byte
-		want        []byte
+		in   []byte
+		want []byte
 	}{
-		{1024, 1024, 64, []byte("test value"), []byte("test value")},
+		{[]byte("test value"), []byte("test value")},
 	} {
-		cache := New(OptionMaxRecords(c.recordCount), OptionRecordSize(c.recordSize), OptionMaxShards(c.shardCount))
+		cache := New()
 		for i := uint32(0); i < 1000; i++ {
 			bs := make([]byte, 4)
-    		binary.LittleEndian.PutUint32(bs, i)
+			binary.LittleEndian.PutUint32(bs, i)
 			if err := cache.Set(bs, c.in, 0); err != nil {
 				t.Errorf("Set error: %s", err.Error())
 			}
 		}
 
 		bs := make([]byte, 4)
-    	binary.LittleEndian.PutUint32(bs, 0)
+		binary.LittleEndian.PutUint32(bs, 0)
 		value, err := cache.Get(bs)
 		if err != nil {
 			t.Errorf("Get error: %s", err.Error())
@@ -68,15 +111,15 @@ func TestCacheIntermediate(t *testing.T) {
 
 func TestCacheDataError(t *testing.T) {
 	for i, c := range []struct {
-		recordCount uint32
-		recordSize  uint32
-		shardCount  uint32
-		in          []byte
-		want        []byte
+		recordSizeSmall  uint32
+		recordSizeMedium uint32
+		recordSizeLarge  uint32
+		in               []byte
+		want             []byte
 	}{
-		{4096, 1, 2048, []byte{0, 1, 2}, []byte{0}},
+		{1, 2, 3, []byte{0, 1, 2, 3, 4, 5}, []byte{0}},
 	} {
-		cache := New(OptionMaxRecords(c.recordCount), OptionRecordSize(c.recordSize), OptionMaxShards(c.shardCount))
+		cache := New(OptionRecordSizeSmall(c.recordSizeSmall), OptionRecordSizeMedium(c.recordSizeMedium), OptionRecordSizeLarge(c.recordSizeLarge))
 		if err := cache.Set([]byte{byte(i)}, c.in, 0); err == nil {
 			t.Errorf("Expecting error 'errDataLimit'")
 		}
@@ -84,7 +127,7 @@ func TestCacheDataError(t *testing.T) {
 }
 
 func TestCacheFreeAfterExpiration(t *testing.T) {
-	cache := New(OptionMaxRecords(512), OptionRecordSize(2048), OptionMaxShards(48), OptionGcStarter(1))
+	cache := New(OptionGcStarter(1))
 
 	cache.Set([]byte("key"), []byte("data"), 500*time.Millisecond)
 	time.Sleep(100 * time.Millisecond)
@@ -102,29 +145,17 @@ func TestCacheFreeAfterExpiration(t *testing.T) {
 	}
 }
 
-func benchmarkCacheNew(recordCount, recordSize, shardCount uint32, b *testing.B) {
+func benchmarkCacheNew(recordCount uint32, b *testing.B) {
 	b.ReportAllocs()
 
 	for n := 0; n < b.N; n++ {
-		New(OptionMaxRecords(recordCount), OptionRecordSize(recordSize), OptionMaxShards(shardCount))
+		New(OptionMaxRecords(recordCount))
 	}
 }
 
-func BenchmarkCacheNewSmall(b *testing.B) {
-	benchmarkCacheNew(512, 2048, 48, b)
-}
-
-func BenchmarkCacheNewMedium(b *testing.B) {
-	benchmarkCacheNew(2048, 2048, 128, b)
-}
-
-func BenchmarkCacheNewLarge(b *testing.B) {
-	benchmarkCacheNew(16384, 4096, 256, b)
-}
-
-func benchmarkCacheSet(recordCount, recordSize, shardCount, dataSize uint32, b *testing.B) {
+func benchmarkCacheSet(recordCount, dataSize uint32, b *testing.B) {
 	var data []byte
-	cache := New(OptionMaxRecords(recordCount), OptionRecordSize(recordSize), OptionMaxShards(shardCount))
+	cache := New(OptionMaxRecords(recordCount))
 
 	for i := uint32(0); i < dataSize; i++ {
 		data = append(data, 1)
@@ -138,62 +169,219 @@ func benchmarkCacheSet(recordCount, recordSize, shardCount, dataSize uint32, b *
 	}
 }
 
+func benchmarkCacheGet(recordCount, dataSize uint32, b *testing.B) {
+	var data []byte
+	cache := New(OptionMaxRecords(recordCount))
+
+	for i := uint32(0); i < dataSize; i++ {
+		data = append(data, 1)
+	}
+
+	for i := 0; i < 128000; i++ {
+		cache.Set([]byte{byte(i)}, data, 0)
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	rand.Seed(42)
+
+	for n := 0; n < b.N; n++ {
+		cache.Get([]byte{byte(rand.Intn(128000))})
+	}
+}
+
+func benchmarkAdvanced(recordCount, dataSize uint32, b *testing.B) {
+	var data []byte
+	cache := New(OptionMaxRecords(recordCount))
+
+	for i := uint32(0); i < dataSize; i++ {
+		data = append(data, 1)
+	}
+
+	cache.Set([]byte{byte(1)}, data, 0)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		cache.Get([]byte{byte(1)})
+	}
+}
+
+func BenchmarkCacheNewSmall(b *testing.B) {
+	benchmarkCacheNew(512, b)
+}
+
+func BenchmarkCacheNewMedium(b *testing.B) {
+	benchmarkCacheNew(2048, b)
+}
+
+func BenchmarkCacheNewLarge(b *testing.B) {
+	benchmarkCacheNew(16384, b)
+}
+
 func BenchmarkCacheSetSmall(b *testing.B) {
-	benchmarkCacheSet(512, 2048, 48, 1024, b)
+	benchmarkCacheSet(512, 1024, b)
 }
 
 func BenchmarkCacheSetMedium(b *testing.B) {
-	benchmarkCacheSet(2048, 2048, 128, 1024, b)
+	benchmarkCacheSet(2048, 1024, b)
 }
 
 func BenchmarkCacheSetLarge(b *testing.B) {
-	benchmarkCacheSet(16384, 4096, 256, 1024, b)
-}
-
-func benchmarkCacheGet(recordCount, recordSize, shardCount, dataSize uint32, b *testing.B) {
-	var data []byte
-	cache := New(OptionMaxRecords(recordCount), OptionRecordSize(recordSize), OptionMaxShards(shardCount))
-
-	for i := uint32(0); i < dataSize; i++ {
-		data = append(data, 1)
-	}
-
-	cache.Set([]byte{byte(1)}, data, 0)
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	for n := 0; n < b.N; n++ {
-		cache.Get([]byte{byte(1)})
-	}
+	benchmarkCacheSet(16384, 1024, b)
 }
 
 func BenchmarkCacheGetSmall(b *testing.B) {
-	benchmarkCacheGet(512, 2048, 48, 1024, b)
+	benchmarkCacheGet(512, 1024, b)
 }
 
 func BenchmarkCacheGetMedium(b *testing.B) {
-	benchmarkCacheGet(2048, 2048, 128, 1024, b)
+	benchmarkCacheGet(2048, 1024, b)
 }
 
 func BenchmarkCacheGetLarge(b *testing.B) {
-	benchmarkCacheGet(16384, 4096, 256, 1024, b)
+	benchmarkCacheGet(16384, 1024, b)
 }
 
-func benchmarkAdvanced(recordCount, recordSize, shardCount, dataSize uint32, b *testing.B) {
-	var data []byte
-	cache := New(OptionMaxRecords(recordCount), OptionRecordSize(recordSize), OptionMaxShards(shardCount))
+const testRecordsCnt = 256000
 
-	for i := uint32(0); i < dataSize; i++ {
-		data = append(data, 1)
-	}
-
-	cache.Set([]byte{byte(1)}, data, 0)
+func BenchmarkAtomicCacheSet(b *testing.B) {
+	cache := newAtoCache()
 
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for n := 0; n < b.N; n++ {
-		cache.Get([]byte{byte(1)})
+		cache.Set([]byte{byte(n)}, []byte("Testing data input"), time.Duration(10*time.Minute))
 	}
+}
+
+func BenchmarkBigCacheSet(b *testing.B) {
+	cache := newBigCache()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		cache.Set(strconv.Itoa(n), []byte("Testing data input"))
+	}
+}
+
+func BenchmarkFreeCacheSet(b *testing.B) {
+	cache := newFreCache()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		cache.Set([]byte{byte(n)}, []byte("Testing data input"), 600)
+	}
+}
+
+func BenchmarkHashicorpCacheSet(b *testing.B) {
+	cache := newHasCache()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		cache.Add(n, []byte("Testing data input"))
+	}
+}
+
+func BenchmarkAtomicCacheGet(b *testing.B) {
+	cache := newAtoCache()
+
+	for n := 0; n < testRecordsCnt; n++ {
+		cache.Set([]byte{byte(n)}, []byte("Testing data input"), time.Duration(10*time.Minute))
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	rand.Seed(42)
+
+	for n := 0; n < b.N; n++ {
+		cache.Get([]byte{byte(rand.Intn(testRecordsCnt))})
+	}
+}
+
+func BenchmarkBigCacheGet(b *testing.B) {
+	cache := newBigCache()
+
+	for n := 0; n < testRecordsCnt; n++ {
+		cache.Set(strconv.Itoa(n), []byte("Testing data input"))
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	rand.Seed(42)
+
+	for n := 0; n < b.N; n++ {
+		cache.Get(strconv.Itoa(rand.Intn(testRecordsCnt)))
+	}
+}
+
+func BenchmarkFreeCacheGet(b *testing.B) {
+	cache := newFreCache()
+
+	for n := 0; n < testRecordsCnt; n++ {
+		cache.Set([]byte{byte(n)}, []byte("Testing data input"), 600)
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	rand.Seed(42)
+
+	for n := 0; n < b.N; n++ {
+		cache.Get([]byte{byte(rand.Intn(testRecordsCnt))})
+	}
+}
+
+func BenchmarkHashicorpCacheGet(b *testing.B) {
+	cache := newHasCache()
+
+	for n := 0; n < testRecordsCnt; n++ {
+		cache.Add(n, []byte("Testing data input"))
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	rand.Seed(42)
+
+	for n := 0; n < b.N; n++ {
+		cache.Get(rand.Intn(testRecordsCnt))
+	}
+}
+
+func newAtoCache() *AtomicCache {
+	cache := New()
+	return cache
+}
+
+func newBigCache() *big.BigCache {
+	cache, _ := big.NewBigCache(big.Config{
+		Shards:             128,
+		LifeWindow:         10 * time.Minute,
+		MaxEntriesInWindow: 1000 * 10 * 60,
+		MaxEntrySize:       2048,
+		HardMaxCacheSize:   0,
+	})
+
+	return cache
+}
+
+func newFreCache() *fre.Cache {
+	cache := fre.NewCache(100 * 1024 * 1024)
+	return cache
+}
+
+func newHasCache() *has.Cache {
+	cache, _ := has.New(262144)
+	return cache
 }
